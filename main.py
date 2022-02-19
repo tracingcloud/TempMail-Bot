15a92fed26c0f82d6c4500b1d39a130e80e1e172
Tracing Cloud <tracingcloud@gmail.com>
2022-02-19 08:02:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 from telebot.types import InlineKeyboardButton
 
 # Fillout Here The BotToken it gets from botfather further queries @riz4d 0n telegram
-bot = telebot.TeleBot('Bottoken here!!')
+bot = telebot.TeleBot('5219484832:AAGEighLvlmkZeNfXyFMJ0lfNtHZM9JUzTw')
 
 while True:
     try:
